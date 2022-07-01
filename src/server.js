9fbb946255b2e9b6daa57c375e3461e1317a4c32
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2022-07-01 11:02:22 -0300

    Created user.avatar upload functionality

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require("express-async-errors")
 const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./config/upload")
 
 const express = require("express")
 
@@ -12,6 +13,8 @@ migrationsRun()
 
 const app = express()
 app.use(express.json())
+
+app.use("/files", express.static(uploadConfig.UPLOAD_FOLDER))
 
 app.use(routes)
 
