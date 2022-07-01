22c5f406ff0e04a8332061ef177493534f59893a
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2022-07-01 00:31:35 -0300

    Creating and implementing authentication middleware

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -29,18 +29,18 @@ class UsersController {
 
 	async update(request, response) {
 		const { name, email, password, old_password } = request.body
-		const { id } = request.params
+		const user_id = request.user.id
 
 		const database = await sqliteConnection()
-		const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+		const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
 
-		if (!id) {
+		if (!user_id) {
 			throw new AppError("Usuário não encontrado")
 		}
 
 		const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-		if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+		if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
 			throw new AppError("E-mail já em uso")
 		}
 
@@ -48,7 +48,7 @@ class UsersController {
 		user.email = email ?? user.email
 
 		if (password && !old_password) {
-			throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
+			throw new AppError("Você precisa informar a senha antiga para definir a nova senha", 401)
 		}
 
 		if (password && old_password) {
@@ -68,7 +68,7 @@ class UsersController {
 			password = ?,
 			updated_at = DATETIME('now')
 			WHERE id = ?`,
-			[user.name, user.email, user.password, id]
+			[user.name, user.email, user.password, user_id]
 		)
 
 		return response.json()
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,13 @@ const notesRoutes = require("./notes.routes")
 
 const tagsRoutes = require("./tags.routes")
 
+const sessionsRoutes = require("./sessions.routes")
+
 const routes = Router()
 
 routes.use("/users", usersRoutes)
 routes.use("/notes", notesRoutes)
 routes.use("/tags", tagsRoutes)
+routes.use("/sessions", sessionsRoutes)
 
 module.exports = routes;
